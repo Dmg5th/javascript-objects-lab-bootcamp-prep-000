0ff6eb5adbb38a6189b6f86b1c88912fe5626c55
Daniel Gould <dmgould310@gmail.com>
2020-07-22 14:01:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-let recipes = {}
-
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({},object,{[key]: value})
+  return Object.assign({}, object, {[key] : value});
 }
-
-function destructivelyDeleteFromObjectByKey(object, key){
-object[key] = value
-return object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object
 }
-
 function deleteFromObjectByKey(object, key){
-  let newObj =  Object.assign({},object)
-  delete newObj[key]
-  return newObj
+  let newObject = Object.assign({},object)
+  delete newObject[key]
+  return newObject
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
+  delete object[key];
   return object
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- object[key] = value
-return object
-}
+
+
+
+
+
+
+
+
