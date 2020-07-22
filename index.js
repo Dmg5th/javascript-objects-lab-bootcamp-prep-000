693828e51a00bbc8e5fd13a3d5227d5fd9635d97
Daniel Gould <dmgould310@gmail.com>
2020-07-22 14:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object 
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
 
 
 
